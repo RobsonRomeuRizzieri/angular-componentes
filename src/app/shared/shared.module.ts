536a73fc72c068d9efe1c2cb3145882ee2e48b81
Robson Romeu Rizzieri <robsonromeu@gmail.com>
2019-01-16 07:07:25 -0200

    criado o componente breadCrumb e alterado os templates para eles usarem esse componente

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from "@angular/router";
+import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BreadCrumbComponent    
+  ],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule
   ],
   exports: [
     //Todos os módulos exportados são conhecidos porquem importar o módulo
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    //Para que todos os módulos tenham acesso ao componente
+    BreadCrumbComponent,
+    RouterModule
   ]
 })
 export class SharedModule { }
